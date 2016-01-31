c0bedacedebb1b4b989278f150db936100704227
Graeme Cooper <Graeme Cooper>
2016-01-31 20:06:19 +1100

    Includes OpenLDAP with dummy data.

--- scripts/install-ldap.sh
+++ scripts/install-ldap.sh
@@ -1,29 +1,22 @@
 yum -y install openldap-clients openldap-servers
-cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
-chown ldap. /var/lib/ldap/DB_CONFIG
 systemctl start slapd
 systemctl enable slapd
-# create a password hash and store it in passphrase.txt
-slappasswd -s vagrant > passphrase.txt
-passwd=$(cat passphrase.txt)
-# add the password hash to the ldif file for changing the root password
-sed -i "s,olcRootPW:.*,olcRootPW: $passwd," /tmp/chrootpw.ldif
-# run ldapadd to change the root password
+# run ldapadd to change the root password to vagrant
 ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/chrootpw.ldif
 # add common schemas used for storing people
 # cosine schema is a dependency of most other schemas
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
-# nis swchema is a network schema which is a depemndency of inetorgperson
+# nis swchema is a network schema which is a dependency of inetorgperson
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
 # inetorperson is the main schema for storing personal details
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
-rm passphrase.txt
-# recreate a password hash for the domain password and store it in passphrase.txt
-slappasswd -s vagrant > passphrase.txt
-passwd=$(cat passphrase.txt)
-# set the password hash in the chdomain file that is used to set up the domain manager
-sed -i "s,olcRootPW:.*,olcRootPW: $passwd," /tmp/chdomain.ldif
-# setup the default domain across the config
+# eduPerson201310 is a schema used to identify attributes of people in education facilities
+ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/eduPerson201310.ldif
+# setup the default domain
 ldapmodify -Y EXTERNAL -H ldapi:/// -f /tmp/chdomain.ldif
-# setup the base domain for the schema
-ldapadd -x -D cn=Manager,dc=server,dc=world -w vagrant -f /tmp/basedomain.ldif
+# add the base organization unit
+ldapadd -x -w vagrant -D cn=Manager,dc=lotj,dc=com,dc=au -f /tmp/base.ldif
+# add the people organization unit
+ldapmodify -H ldapi:/// -x -w vagrant -D cn=Manager,dc=lotj,dc=com,dc=au -f /tmp/people.ldif
+# add some people to the directory
+#ldapmodify -H ldapi:/// -x -w vagrant -D cn=Manager,dc=lotj,dc=com,dc=au -f /tmp/sample.ldif
--- scripts/shibboleth-idp.sh
+++ scripts/shibboleth-idp.sh
@@ -1,11 +1,51 @@
 wget -O /tmp/shibboleth-identity-provider-3.2.1.tar.gz https://shibboleth.net/downloads/identity-provider/latest/shibboleth-identity-provider-3.2.1.tar.gz
 mkdir /home/vagrant/shibboleth-idp3
+echo "Created shibboleth source directory"
 tar -xf /tmp/shibboleth-identity-provider-3.2.1.tar.gz -C /home/vagrant/shibboleth-idp3 --strip-components=1
-wget https://build.shibboleth.net/nexus/service/local/repositories/thirdparty/content/javax/servlet/jstl/1.2/jstl-1.2.jar
+echo "Replacing build.xml to allow unattended installation."
+cp /tmp/build.xml /home/vagrant/shibboleth-idp3/bin/build.xml
+wget -O /tmp/jstl-1.2.jar https://build.shibboleth.net/nexus/service/local/repositories/thirdparty/content/javax/servlet/jstl/1.2/jstl-1.2.jar
 mkdir /opt/shibboleth-idp
-cd /home/vagrant/shibboleth-idp3
-./bin/install.sh -Didp.target.dir=/opt/shibboleth-idp -Didp.keystore.password=vagrant -Didp.sealer.password=vagrant
+if [ -d /opt/shibboleth-idp ]
+	then
+		echo "Created shibboleth target directory at /opt/shibboleth-idp."
+fi
+if [ -f /tmp/idp.properties ]
+	then
+		hostname = $(hostname)	
+		sed -i "/s/idp.host.name =.*/idp.host.name = $hostname/"
+		if [ $? -eq 0 ]
+			then
+				echo "Changed hostname in idp.properties"
+		fi
+fi
+/home/vagrant/shibboleth-idp3/bin/install.sh -Didp.property.file='/tmp/idp.properties'
+if [ -d /opt/shibboleth-idp/bin ]
+	then
+		echo "Installed shibboleth idp to /opt/shibboleth-idp"
+	else
+		exit 3
+fi
 mv /tmp/idp.xml /opt/tomcat/conf/Catalina/localhost/idp.xml
-mv jsttl1-1.2.jar /opt/shibboleth-idp/edit-webapp/WEB-INF/lib/
-cd /opt/shibboleth-idp
-./bin/install.sh
+if [ $? -eq 0 ]
+	then
+		echo "Added shibboleth-idp war location to Tomcat"
+	else
+		exit 4
+fi
+mv /tmp/jstl-1.2.jar /opt/shibboleth-idp/edit-webapp/WEB-INF/lib/
+if [ $? -eq 0 ]
+	then
+		echo "Adding JSP support to shibboleth-idp ..."
+		/opt/shibboleth-idp/bin/build.sh
+		if [ $? -eq 0 ]
+			then
+				echo "Included jsp support in shibboleth idp"
+			else
+				exit 3
+		fi
+	else
+		exit 4
+fi
+echo "...Successfully completed shibboleth installation!"
+exit 0
